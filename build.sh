33294cbc11f2f9b2cf1847104e01ad072291b7bc
Marcus <marcusowlvl1@gmail.com>
2026-07-02 12:16:24 -0300

    fix: move build.sh to backend/

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Instala dependências
-pip install -r requirements.txt
-
-# Coleta arquivos estáticos
-python manage.py collectstatic --noinput
-
-# Migrações (se houver)
-python manage.py migrate
